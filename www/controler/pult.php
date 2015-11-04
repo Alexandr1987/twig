d6aff7a55274dcbf9b74f0d2b35e2fe76115dc58
Alexandr <advokat-1987@yandex.ru>
2015-11-04 23:57:10 +0200

    ДЗ от 04.11

--- www/controler/pult.php
+++ www/controler/pult.php
@@ -7,7 +7,7 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem([__DIR__.'/../templates',
     __DIR__.'/../layouts']);
 $twig = new Twig_Environment($loader,[
-    'cache' => __DIR__.'/../cash',
+    'cache' == false/*=> __DIR__.'/../cash',**/
 ]);
 
 
@@ -16,11 +16,33 @@ $twig = new Twig_Environment($loader,[
 
 if(!empty($_GET['id'])) {
     if ($_GET['id'] == 3) {
-        echo $twig->render('pagegalery.html');
+        echo $twig->render('pagetovars.html',[
+            'tegs' => [
+                ['name' => 'tv'],
+                ['name' => 'mobile'],
+                ['name' => 'phone'],
+                ['name' => 'pk']
+            ]]);
     } elseif ($_GET['id'] == 2) {
         echo $twig->render('pageabout.html');
-    } elseif ($_GET['id'] == 1) {
-        echo $twig->render('page.html');
-    };
-}else {echo $twig->render('page.html');};
+    }elseif ($_GET['id'] == 4) {
+        echo $twig->render('maintovarpage.html',[
+            'tegs' => [
+                ['name' => 'tv'],
+                ['name' => 'mobile'],
+                ['name' => 'phone'],
+                ['name' => 'pk']
+            ],
+            ]);
+    }
+}else  {
+    echo $twig->render('page.html',[
+        'users' => [
+            10 =>['name' => 'Ivan Petrov'],
+            22 =>['name' => 'Ivan Sidorov'],
+            24 =>['name' => 'Anton Petrov'],
+            19 =>['name' => 'Alex Petrov']
+        ],
+    ]);
+};
 
