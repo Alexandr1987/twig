a76cc271d292f83998ca16f5db9cc5ba5acbbde1
Alexandr <advokat-1987@yandex.ru>
2015-11-07 21:17:16 +0200

    ДЗ от 07.11

---
+++ www/class/Exchange.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Александр
+ * Date: 07.11.2015
+ * Time: 19:16
+ */
+
+class Exchange
+    extends Twig_Extension
+{
+
+    public function getName(){
+        return 'exchange';
+    }
+
+
+}
