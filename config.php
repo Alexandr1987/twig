bcbe3e74b0bd4e8d79acb45e00b262686facae2f
Alexandr <advokat-1987@yandex.ru>
2015-11-07 21:18:37 +0200

    ДЗ от 07.11

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Александр
+ * Date: 07.11.2015
+ * Time: 12:09
+ */
+
+return ['title' => 'Наше приложение',
+        'company' => 'WebShulz',
+        'phone' => '09854503850',
+        'adress' => 'г.Одесса, ул.Дерибассовкая, 12',
+        ];
